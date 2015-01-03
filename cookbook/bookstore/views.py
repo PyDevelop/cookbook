6f49a45590b915bb2be9fa1cf12482f3a621a49b
Andres Pinto <andres.consultas@gmail.com>
2015-01-03 18:31:04 -0300

    admin and principal done

--- cookbook/bookstore/views.py
+++ cookbook/bookstore/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def principal(request):
+    render(request,"principal.html")
+    
+@login_required    
+def administrator(request):
+    render(request,"administration.html")
+    
