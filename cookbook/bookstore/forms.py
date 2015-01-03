c661069669f9e5b85cad3bbf0e7e05a867ca2199
Andres Pinto <andres.consultas@gmail.com>
2015-01-02 23:14:13 -0300

    forms added

--- cookbook/bookstore/forms.py
+++ cookbook/bookstore/forms.py
@@ -5,3 +5,22 @@ class EditorForm(forms.Form):
     class Meta:
         model = Editor
         exclude = ('status',)
+        
+class AuthorForm(forms.Form):
+    class Meta:
+        model = Author
+        exclude = ('status',)
+        
+    
+class CategoryForm(forms.Form):
+    class Meta:
+        model = Category
+        exclude = ('status',)
+        
+        
+class BookForm(forms.Form):
+    class Meta:
+        model = Book
+        exclude = ('status',)
+        
+        
