5190ca86d755f3390a80b9439642cadb15b43ae1
PyDevelop <someoneprogramming@gmail.com>
2015-01-05 03:39:35 -0300

    categories management

--- cookbook/bookstore/views.py
+++ cookbook/bookstore/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.forms import *
+
 # Create your views here.
 
 def principal(request):
@@ -8,4 +10,20 @@ def principal(request):
 @login_required    
 def administrator(request):
     render(request,"administration.html")
+    
+
+#book -editor - category -author
+
+
+#Category management
+    
+def addCategory(request):
+    form = CategoryForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        message = "New category added successufully"
+    else:
+        message = form.errors
+    return render(request, "addCategory.html",  )
+        
     
