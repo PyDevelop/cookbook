1c7c46e1e858381a194f4125d68023a430b1a228
PyDevelop <someoneprogramming@gmail.com>
2015-01-05 15:11:38 -0300

    new urls added

--- cookbook/bookstore/urls.py
+++ cookbook/bookstore/urls.py
@@ -3,7 +3,16 @@ from bookstore import views
 
 #patron -vista que lo atiende-nombre
 urlpatterns = patterns('bookstore.views',
+                       #main page
                        url(r'^principal/',"mainPage",name="index"),
+                       
+                       #listing views
+                       url(r'^bookcatalog',"listBooks",name="catalog"),
+                       url(r'^authorList',"listAuthors",name="authors"),
+                       url(r'^editorList',"listEditors",name="editors"),
+                       url(r'^categories',"listCategories",name="categories"),
+                       
+                       
                        url(r'^admnistration/',"adminPage",name="administrator"),
                       
                       
