9994d6d0bb9b860553e374c26dc808cc83c59d79
PyDevelop <someoneprogramming@gmail.com>
2015-01-05 15:25:21 -0300

    new urls added

--- cookbook/bookstore/urls.py
+++ cookbook/bookstore/urls.py
@@ -12,6 +12,23 @@ urlpatterns = patterns('bookstore.views',
                        url(r'^editorList',"listEditors",name="editors"),
                        url(r'^categories',"listCategories",name="categories"),
                        
+                       #adding views
+                       url(r'^addbook',"addBook",name="addBook"),
+                       url(r'^addAuthor',"addAuthor",name="addAuthor"),
+                       url(r'^addEditor',"addEditor",name="addEditor"),
+                       url(r'^addCategory',"addCategory",name="addCategory"),
+                       
+                       #deleting views
+                       url(r'^deleteBook/(?P<pk>\d+)',"deleteBook",name="deleteBook"),
+                       url(r'^deleteAuthor/(?P<pk>\d+)',"deleteAuthor",name="deleteAuthor"),
+                       url(r'^deleteEditor/(?P<pk>\d+)',"deleteEditor",name="deleteEditor"),
+                       url(r'^deleteCategory/(?P<pk>\d+)',"deleteCategory",name="deleteCategory"),
+                       
+                       #changing views
+                       url(r'^changeBook/(?P<pk>\d+)',"changeBook",name="changeBook"),
+                       url(r'^changeAuthor/(?P<pk>\d+)',"changeAuthor",name="changeAuthor"),
+                       url(r'^changeEditor/(?P<pk>\d+)',"changeEditor",name="changeEditor"),
+                       url(r'^changeCategory/(?P<pk>\d+)',"changeCategory",name="changeCategory"),
                        
                        url(r'^admnistration/',"adminPage",name="administrator"),
                       
