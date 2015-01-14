27211da97acf521a86aa2f52875ffa79d14ba735
Andres Pinto <andres.consultas@gmail.com>
2015-01-14 19:08:51 -0300

    dependencies field now is hidden in user forms

--- cookbook/bookstore/forms.py
+++ cookbook/bookstore/forms.py
@@ -4,24 +4,24 @@ from bookstore.models import *
 class EditorForm(ModelForm):
     class Meta:
         model = Editor
-        exclude = ('status',)
+        exclude = ('status','dependecies',)
         
 class AuthorForm(ModelForm):
     class Meta:
         model = Author
-        exclude = ('status',)
+        exclude = ('status','dependecies',)
         
     
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = "__all__"
-        exclude = ('status',)
+        exclude = ('status','dependecies',)
         
         
 class BookForm(ModelForm):
     class Meta:
         model = Book
-        exclude = ('status',)
+        exclude = ('status','dependecies',)
         
         
