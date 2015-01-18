f71c66305eb354e724cdeecf5296035bab222704
Andres Pinto <andres.consultas@gmail.com>
2015-01-18 17:04:32 -0300

    mistake: dependencies instead dependecies in model forms statement

--- cookbook/bookstore/forms.py
+++ cookbook/bookstore/forms.py
@@ -4,24 +4,24 @@ from bookstore.models import *
 class EditorForm(ModelForm):
     class Meta:
         model = Editor
-        exclude = ('status','dependecies',)
+        exclude = ('status','dependencies',)
         
 class AuthorForm(ModelForm):
     class Meta:
         model = Author
-        exclude = ('status','dependecies',)
+        exclude = ('status','dependencies',)
         
     
 class CategoryForm(ModelForm):
     class Meta:
         model = Category
         fields = "__all__"
-        exclude = ('status','dependecies',)
+        exclude = ('status','dependencies',)
         
         
 class BookForm(ModelForm):
     class Meta:
         model = Book
-        exclude = ('status','dependecies',)
+        exclude = ('status','dependencies',)
         
         
